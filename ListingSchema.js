53fe18ff3b8ab1e05953aebf18d1294674620547
Caleb Bryant <cal2u@ufl.edu>
2017-09-04 23:26:02 -0400

    Finished Assignment 3

--- ListingSchema.js
+++ ListingSchema.js
@@ -1,15 +1,32 @@
 /* Import mongoose and define any variables needed to create the schema */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 /* Create your schema */
 var listingSchema = new Schema({
-  /* your code here */
+  name: { type: String, required: true, unique: true },
+  code: { type: String, required: true, unique: true },
+  address: String,
+  coordinates: {
+    latitude: Number,
+    longitude: Number,
+  },
+  created_at: Date,
+  updated_at: Date
 });
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
 listingSchema.pre('save', function(next) {
-  /* your code here */
+  // get the current time
+  var now = new Date();
+
+  // change the updated_at field to the current time
+  this.updated_at = now;
+
+  // if created_at doesn't exist, assign the current time to that field
+  if (!this.created_at)
+    this.created_at = now;
+  next();
 });
 
 /* Use your schema to instantiate a Mongoose model */
