c75e9f4c28d4e760206647519d39cb905d83b91d
Caleb Bryant <cal2u@ufl.edu>
2017-09-19 17:11:47 -0400

    Finished assignment 5 code

--- client/app/controllers/listings.client.controller.js
+++ client/app/controllers/listings.client.controller.js
@@ -1,4 +1,4 @@
-angular.module('listings').controller('ListingsController', ['$scope', '$location', '$stateParams', '$state', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', '$location', '$stateParams', '$state', 'Listings',
   function($scope, $location, $stateParams, $state, Listings){
     $scope.find = function() {
       /* set loader*/
@@ -8,6 +8,21 @@ angular.module('listings').controller('ListingsController', ['$scope', '$locatio
       Listings.getAll().then(function(response) {
         $scope.loading = false; //remove loader
         $scope.listings = response.data;
+
+        $scope.markers = [];
+        for (var i = 0; i < $scope.listings.length; i++) {
+          var listing = $scope.listings[i];
+          if (listing.coordinates) {
+            $scope.markers.push({
+              latitude: listing.coordinates.latitude,
+              longitude: listing.coordinates.longitude,
+              id: i,
+              name: listing.name,
+              code: listing.code,
+              address: listing.address
+            });
+          }
+        }
       }, function(error) {
         $scope.loading = false;
         $scope.error = 'Unable to retrieve listings!\n' + error;
@@ -19,16 +34,16 @@ angular.module('listings').controller('ListingsController', ['$scope', '$locatio
       $scope.loading = true;
 
       /*
-        Take a look at 'list-listings.client.view', and find the ui-sref attribute that switches the state to the view 
-        for a single listing. Take note of how the state is switched: 
+        Take a look at 'list-listings.client.view', and find the ui-sref attribute that switches the state to the view
+        for a single listing. Take note of how the state is switched:
 
           ui-sref="listings.view({ listingId: listing._id })"
 
         Passing in a parameter to the state allows us to access specific properties in the controller.
 
-        Now take a look at 'view-listing.client.view'. The view is initialized by calling "findOne()". 
-        $stateParams holds all the parameters passed to the state, so we are able to access the id for the 
-        specific listing we want to find in order to display it to the user. 
+        Now take a look at 'view-listing.client.view'. The view is initialized by calling "findOne()".
+        $stateParams holds all the parameters passed to the state, so we are able to access the id for the
+        specific listing we want to find in order to display it to the user.
        */
 
       var id = $stateParams.listingId;
@@ -37,16 +52,16 @@ angular.module('listings').controller('ListingsController', ['$scope', '$locatio
               .then(function(response) {
                 $scope.listing = response.data;
                 $scope.loading = false;
-              }, function(error) {  
+              }, function(error) {
                 $scope.error = 'Unable to retrieve listing with id "' + id + '"\n' + error;
                 $scope.loading = false;
               });
-    };  
+    };
 
     $scope.create = function(isValid) {
       $scope.error = null;
 
-      /* 
+      /*
         Check that the form is valid. (https://github.com/paulyoder/angular-bootstrap-show-errors)
        */
       if (!isValid) {
@@ -57,8 +72,8 @@ angular.module('listings').controller('ListingsController', ['$scope', '$locatio
 
       /* Create the listing object */
       var listing = {
-        name: $scope.name, 
-        code: $scope.code, 
+        name: $scope.name,
+        code: $scope.code,
         address: $scope.address
       };
 
@@ -74,17 +89,47 @@ angular.module('listings').controller('ListingsController', ['$scope', '$locatio
     };
 
     $scope.update = function(isValid) {
+      $scope.error = null;
+
       /*
-        Fill in this function that should update a listing if the form is valid. Once the update has 
-        successfully finished, navigate back to the 'listing.list' state using $state.go(). If an error 
-        occurs, pass it to $scope.error. 
+        Check that the form is valid. (https://github.com/paulyoder/angular-bootstrap-show-errors)
        */
+      if (!isValid) {
+        $scope.$broadcast('show-errors-check-validity', 'articleForm');
+        return false;
+      }
+
+      /*
+        Fill in this function that should update a listing if the form is valid. Once the update has
+        successfully finished, navigate back to the 'listing.list' state using $state.go(). If an error
+        occurs, pass it to $scope.error.
+       */
+
+       /* Save the article using the Listings factory */
+       Listings.update($scope.listing._id, $scope.listing)
+               .then(function(response) {
+                 //if the object is successfully saved redirect back to the list page
+                 $state.go('listings.list', { successMessage: 'Listing succesfully updated!' });
+               }, function(error) {
+                 //otherwise display the error
+                 $scope.error = 'Unable to update listing!\n' + error;
+               });
     };
 
     $scope.remove = function() {
+      $scope.error = null;
+
       /*
-        Implement the remove function. If the removal is successful, navigate back to 'listing.list'. Otherwise, 
-        display the error. 
+        Check that the form is valid. (https://github.com/paulyoder/angular-bootstrap-show-errors)
+       */
+      if (!isValid) {
+        $scope.$broadcast('show-errors-check-validity', 'articleForm');
+
+        return false;
+      }
+      /*
+        Implement the remove function. If the removal is successful, navigate back to 'listing.list'. Otherwise,
+        display the error.
        */
     };
 
@@ -98,8 +143,13 @@ angular.module('listings').controller('ListingsController', ['$scope', '$locatio
       center: {
         latitude: 29.65163059999999,
         longitude: -82.3410518
-      }, 
+      },
       zoom: 14
     }
+
+    $scope.onClick = function(marker, eventName, model) {
+        console.log("Clicked!");
+        model.show = !model.show;
+    };
   }
 ]);
